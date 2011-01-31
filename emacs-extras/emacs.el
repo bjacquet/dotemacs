72c3f349c0b3f212620635930b451a4fd0bca9fd
Bruno Jacquet <bruno.jacquet@gmail.com>
2011-01-31 13:13:11 +0000

    Dropped MEW Mode, didn't enjoyed that much. Added W3M Mode and GNUS Mode file.

--- emacs-extras/emacs.el
+++ emacs-extras/emacs.el
@@ -196,77 +196,16 @@ or to \[buffer-name if it has no file"
 
 
 ;;; ---------------------------------------------------------------------
-;;; GNUS
-;;;
-(setq gnus-select-method '(nntp "news.gmane.org"))
-(setq gnus-read-newsrc-file nil)
-(add-hook 'gnus-summary-mode-hook 'my-setup-hl-line)
-(add-hook 'gnus-group-mode-hook 'my-setup-hl-line)
-
-(defun my-setup-hl-line ()
- (hl-line-mode 1)
- (setq cursor-type nil))
-
-;; http://groups.google.com/group/gnu.emacs.gnus/browse_thread/thread/a673a74356e7141f
-(when window-system
- (setq gnus-sum-thread-tree-indent "  ")
- (setq gnus-sum-thread-tree-root "● ")
- (setq gnus-sum-thread-tree-false-root "◯ ")
- (setq gnus-sum-thread-tree-single-indent "◎ ")
- (setq gnus-sum-thread-tree-vertical        "│")
- (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
- (setq gnus-sum-thread-tree-single-leaf     "╰─► ")
- (setq gnus-user-date-format-alist '(;; ((gnus-seconds-today)
-                                     ;;  . "%k:%M")
-                                     ;; (604800 . "%a %k:%M")
-                                     ;; ((gnus-seconds-month)
-                                     ;;  . "%a %d")
-                                     ((gnus-seconds-year)
-                                      . "%d %b '%y")
-                                     (t . "%d %b '%y"))))
-(setq gnus-summary-line-format
-     (concat
-      "%0{%U%R%z%}"
-      "%3{│%}" "%1{%&user-date;%}" "%3{│%}" ;; date
-      "  "
-      "%4{%-15,15f%}"               ;; name
-      "  "
-      "%3{│%}"
-      " "
-      "%1{%B%}"
-      "%s\n"))
-(setq gnus-summary-display-arrow t)
-(gnus-add-configuration
- '(summary
-   (vertical 1.0
-	       (horizontal 0.25
-			   [group 0.30]
-			   [summary 1.0 point])
-	       (vertical 1.0
-			 [article 1.0]))))
-
-
-;;; ---------------------------------------------------------------------
-;;; MEW
+;;; W3M
 ;;;
 (unless (string-equal host "SETUBAL") ; SISCOG
-  (autoload 'mew "mew" nil t)
-  (autoload 'mew-send "mew" nil t)
-  ;; Optional setup (Read Mail menu for Emacs 21):
-  (if (boundp 'read-mail-command)
-      (setq read-mail-command 'mew))
-  ;; Optional setup (e.g. C-xm for sending a message):
-  (autoload 'mew-user-agent-compose "mew" nil t)
-  (if (boundp 'mail-user-agent)
-      (setq mail-user-agent 'mew-user-agent))
-  (if (fboundp 'define-mail-user-agent)
-      (define-mail-user-agent
-	'mew-user-agent
-	'mew-user-agent-compose
-	'mew-draft-send-message
-	'mew-draft-kill
-	'mew-send-hook)))
+  (load-file (expand-file-name (concat emacs-extras-d "/w3m.el"))))
 
+;;; ---------------------------------------------------------------------
+;;; GNUS
+;;;
+(unless (string-equal host "SETUBAL") ; SISCOG
+  (load-file (expand-file-name (concat emacs-extras-d "/gnus.el"))))
 
 
 ;;; ---------------------------------------------------------------------
---
+++ emacs-extras/gnus.el
@@ -0,0 +1,49 @@
+;;; ---------------------------------------------------------------------
+;;; GNUS
+;;;
+(setq gnus-select-method '(nntp "news.gmane.org"))
+(setq gnus-read-newsrc-file nil)
+(add-hook 'gnus-summary-mode-hook 'my-setup-hl-line)
+(add-hook 'gnus-group-mode-hook 'my-setup-hl-line)
+
+(defun my-setup-hl-line ()
+ (hl-line-mode 1)
+ (setq cursor-type nil))
+
+;; http://groups.google.com/group/gnu.emacs.gnus/browse_thread/thread/a673a74356e7141f
+(when window-system
+ (setq gnus-sum-thread-tree-indent "  ")
+ (setq gnus-sum-thread-tree-root "● ")
+ (setq gnus-sum-thread-tree-false-root "◯ ")
+ (setq gnus-sum-thread-tree-single-indent "◎ ")
+ (setq gnus-sum-thread-tree-vertical        "│")
+ (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+ (setq gnus-sum-thread-tree-single-leaf     "╰─► ")
+ (setq gnus-user-date-format-alist '(;; ((gnus-seconds-today)
+                                     ;;  . "%k:%M")
+                                     ;; (604800 . "%a %k:%M")
+                                     ;; ((gnus-seconds-month)
+                                     ;;  . "%a %d")
+                                     ((gnus-seconds-year)
+                                      . "%d %b '%y")
+                                     (t . "%d %b '%y"))))
+(setq gnus-summary-line-format
+     (concat
+      "%0{%U%R%z%}"
+      "%3{│%}" "%1{%&user-date;%}" "%3{│%}" ;; date
+      "  "
+      "%4{%-15,15f%}"               ;; name
+      "  "
+      "%3{│%}"
+      " "
+      "%1{%B%}"
+      "%s\n"))
+(setq gnus-summary-display-arrow t)
+(gnus-add-configuration
+ '(summary
+   (vertical 1.0
+	       (horizontal 0.25
+			   [group 0.30]
+			   [summary 1.0 point])
+	       (vertical 1.0
+			 [article 1.0]))))
