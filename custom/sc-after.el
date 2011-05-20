6b0a421fbc727633ebe1aeee5c0ad33580a79a41
Bruno Jacquet <bruno.jacquet@gmail.com>
2011-05-21 00:26:56 +0100

    Comment out keybindings.

--- custom/sc-after.el
+++ custom/sc-after.el
@@ -26,16 +26,16 @@
 
 ;; Own Keyboard Combinations
 
-(define-key global-map [f1]      'insert-tab)
-(define-key global-map [f2]      'insert-comment-tabs)
-(define-key global-map [f3]      'sc-indent-region)
-(define-key global-map [f4]      'sc-insert-commas2)
+;; (define-key global-map [f1]      'insert-tab)
+;; (define-key global-map [f2]      'insert-comment-tabs)
+;; (define-key global-map [f3]      'sc-indent-region)
+;; (define-key global-map [f4]      'sc-insert-commas2)
 ;; (define-key global-map [f5]      'sc-find-definition)
 ;; (define-key global-map [f6]      'sc-find-string-in-file)
 ;; (define-key global-map [f7]      'upcase-region)
 ;; (define-key global-map [f8]      'downcase-region)
-(define-key global-map [f9]      'insert-keywords)
-(define-key global-map [f10]     'save-buffers-kill-emacs)
+;; (define-key global-map [f9]      'insert-keywords)
+;; (define-key global-map [f10]     'save-buffers-kill-emacs)
 
 ;; (define-key global-map [home]    'beginning-of-line)
 ;; (define-key global-map [C-home]  'beginning-of-buffer)
@@ -45,24 +45,24 @@
 (define-key global-map [M-left]  'backward-sexp)
 (define-key global-map [M-right] 'forward-sexp)
 (define-key global-map "\M-\\"   'find-in-other-window)
-(define-key global-map "\M->"    'search-mod-date-forward)
-(define-key global-map "\M-<"    'search-mod-date-forward)
+;; (define-key global-map "\M->"    'search-mod-date-forward)
+;; (define-key global-map "\M-<"    'search-mod-date-forward)
 
 
 
 ;;;--------------------------------------------------------------------------------
 
-(defun insert-string-elem ()
-  (interactive)
-  (insert-doc-mark "elem"))
+;; (defun insert-string-elem ()
+;;   (interactive)
+;;   (insert-doc-mark "elem"))
 
-(defun insert-string-arg ()
-  (interactive)
-  (insert-doc-mark "arg"))
+;; (defun insert-string-arg ()
+;;   (interactive)
+;;   (insert-doc-mark "arg"))
 
-(defun insert-string-emph ()
-  (interactive)
-  (insert-doc-mark "emph"))
+;; (defun insert-string-emph ()
+;;   (interactive)
+;;   (insert-doc-mark "emph"))
 
 ;; (define-key global-map [f5] 'insert-string-elem )
 ;; (define-key global-map [f6] 'insert-string-arg )
@@ -77,5 +77,5 @@
 (define-key global-map [f11] 'ediff-file-with-original1 )
 
 
-(load-file (expand-file-name "~/emacs/custom/sc-copy-versions.el"))
-;(load-file (expand-file-name "~/emacs/custom/work-track-mode.el"))
+;; (load-file (expand-file-name "~/emacs/custom/sc-copy-versions.el"))
+;; (load-file (expand-file-name "~/emacs/custom/work-track-mode.el"))
