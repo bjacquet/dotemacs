b97502e81b7bcd6a3a0d843e58867f981c20f9f8
Bruno Jacquet <bruno.jacquet@gmail.com>
2011-09-21 23:33:23 +0100

    Added Slime to OSX.

--- emacs-extras/slime.el
+++ emacs-extras/slime.el
@@ -1,5 +1,9 @@
-(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
-(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-20110219-cvs/")  ; your SLIME directory
-(require 'slime-autoloads)
-(slime-setup '(slime-fancy))
 
+(cond ((string-equal (system-name) "owl") 
+       (setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
+       (add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-20110219-cvs/")  ; your SLIME directory
+       (require 'slime-autoloads)
+       (slime-setup '(slime-fancy)))
+      ((string-equal (system-name) "puffin.lan")
+       (load (expand-file-name "~/quicklisp/slime-helper.el"))
+       (setq inferior-lisp-program "~/Desktop/sbcl-1.0.51-x86-darwin/run-sbcl.sh")))
