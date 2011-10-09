e438c46d88161fb4fa8ef2c38f04b711202bce0c
Bruno Jacquet <bruno.jacquet@gmail.com>
2011-10-09 18:45:30 +0100

    Wrote the Installation section in README. Optional modes moved to new folder `load-path'. Renamed the startup files. Decreased the startup loading time.

--- emacs-extras/load-path/dotemacs.el
+++ emacs-extras/load-path/dotemacs.el
@@ -1,5 +1,5 @@
 ;; -*- coding: utf-8; -*-
-;;;; common.el - Emacs Common Utilities
+;;;; dotemacs.el - Emacs Utilities
 
 (setq debug-on-error t)
 
@@ -17,8 +17,11 @@
    ((getenv "USERNAME"))))
 
 
+(setq at-siscog-p (string-equal host "SETUBAL"))
+
+
 (defvar emacs-d
-  (if (string-equal host "SETUBAL")  ; SISCOG 
+  (if at-siscog-p
       "~/emacs"
     "~/dotemacs"))
 
@@ -28,13 +31,13 @@
 
 
 (defvar notes-d
-  (if (string-equal host "SETUBAL")  ; SISCOG 
+  (if at-siscog-p
       "~/Documents/notes"
     "~/flash_drive/notes"))
 
 
 (defvar diary-d
-  (if (string-equal host "SETUBAL")  ; SISCOG 
+  (if at-siscog-p
       "~/Documents/diary"
     "~/flash_drive/diary"))
 
@@ -234,5 +237,64 @@ or to \[buffer-name if it has no file"
 (global-set-key '[M-down]  'pager-row-down)
 
 
-;; common.el EOF
+;;; ---------------------------------------------------------------------
+;;; Auto Complete
+;;;
+(eval-after-load 'auto-complete
+  '(progn
+     (global-auto-complete-mode t)
+     (setq ac-auto-start 3)
+     (setq ac-dwim t)
+     (define-key ac-complete-mode-map "\t" 'ac-complete)
+     (define-key ac-complete-mode-map "\r" nil)
+     (define-key ac-complete-mode-map "\C-n" 'ac-next)
+     (define-key ac-complete-mode-map "\C-p" 'ac-previous)))
 
+
+;;; ---------------------------------------------------------------------
+;;; Stuff
+;;;
+(defun dos-unix ()
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\r" nil t)
+    (replace-match "")))
+
+(defun unix-dos ()
+  (interactive)
+  (goto-char (point-min))
+  (while (search-forward "\n" nil t)
+    (replace-match "\r\n")))
+
+(defun espacos () (interactive)
+  "Eliminate whitespace at enfs of all lines in the buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "[ \t][ \t]*$" nil t)
+      (delete-region (match-beginning 0) (point)))))
+
+(defun abre-emacs-el ()
+  (interactive)
+  (find-file (locate-library "dotemacs.el")))
+
+(defun abre-notes-org ()
+  (interactive)
+  (find-file (expand-file-name "~/flash_drive/notes/note.org"))
+  (find-file (expand-file-name "~/flash_drive/notes/london.org"))
+  (find-file (expand-file-name "~/flash_drive/notes/remember.org")))
+
+(defun trim-str (str)
+  "Trims leading and tailing whitespace from STR."
+  (let ((s (if (symbolp str) (symbol-name str) str)))
+    (replace-regexp-in-string "\\(^[[:space:]\n]*\\|[[:space:]\n]*$\\)" "" s)))
+
+
+;;; ---------------------------------------------------------------------
+;;; Modes loaded on request
+;;;
+(add-to-list 'load-path (concat emacs-extras-d "/load-path"))
+
+
+(provide 'dotemacs)
+
+;; dotemacs.el EOF
