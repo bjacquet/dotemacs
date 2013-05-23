eb161e8583e3e562c95d26f1fd61f357396537eb
B Jacquet <bruno.jacquet@gmail.com>
2013-05-24 00:31:18 +0100

    Added Slime (for home use).

---
+++ pkg-loaders/slime.el
@@ -0,0 +1,7 @@
+;;; ---------------------------------------------------------------------
+;;; Slime - for use at home
+;;;
+
+(load (expand-file-name "~/Documents/Projects/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl");)
