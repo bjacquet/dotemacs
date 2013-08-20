15b93921014387ff7ef4ca708d2473ff2f5e0b0a
Bruno Jacquet <bruno.jacquet@gmail.com>
2013-08-20 23:47:45 +0100

    Updated Magit.

--- pkg-loaders/magit.el
+++ pkg-loaders/magit.el
@@ -1,5 +1,8 @@
 ;;; ---------------------------------------------------------------------
 ;;; Magit
 ;;;
+(when (equal system-type 'darwin)
+  (add-to-list 'exec-path "/usr/local/git/bin/"))
+
 (add-to-list 'load-path (expand-package "magit"))
 (autoload 'magit-status "magit" "Loads magit-mode" t)
