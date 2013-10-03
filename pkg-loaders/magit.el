ef28dc0ebff5104f6f61847cb2d3f8da22864f96
B Jacquet <bjacquet@siscog.pt>
2013-10-03 10:23:17 +0100

    Updated magit and added submodule git-modes.

--- pkg-loaders/magit.el
+++ pkg-loaders/magit.el
@@ -4,5 +4,6 @@
 (when (equal system-type 'darwin)
   (add-to-list 'exec-path "/usr/local/git/bin/"))
 
+(add-to-list 'load-path (expand-package "git-modes"))
 (add-to-list 'load-path (expand-package "magit"))
 (autoload 'magit-status "magit" "Loads magit-mode" t)
