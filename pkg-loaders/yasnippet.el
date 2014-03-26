87527c80aa5194b92eac80c6251e9844f666f185
B Jacquet <bjacquet@siscog.pt>
2014-03-26 14:53:47 +0000

    Minor correction to load Yasnippet package.

--- pkg-loaders/yasnippet.el
+++ pkg-loaders/yasnippet.el
@@ -11,7 +11,8 @@
      (require 'yasnippet)
      (setq yas-snippet-dirs (list (expand-package "yasnippet/snippets/")
 				  (concat emacs-dir "packages/snippets/")))
-     (yas/global-mode 1)
+     (yas-global-mode 1)
      (yas--load-directory-1 (concat emacs-dir "packages/snippets/sc-mode/")
-			    'text-mode
-			    nil)))
+      			    'text-mode)
+     (yas--load-directory-1 (concat emacs-dir "packages/snippets/sc-mode/")
+			    'fundamental-mode)))
