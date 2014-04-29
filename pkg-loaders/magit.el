7732c4b9cc15907d0b7ae916746c91c6eab905ab
B Jacquet <bjacquet@siscog.pt>
2014-04-29 16:09:37 +0100

    dired-x configuration for magit.

--- pkg-loaders/magit.el
+++ pkg-loaders/magit.el
@@ -4,6 +4,13 @@
 (when (equal system-type 'darwin)
   (add-to-list 'exec-path "/usr/local/git/bin/"))
 
+
+(add-hook 'dired-load-hook
+          (lambda ()
+	    (load "dired-x")))
+(add-hook 'dired-mode-hook
+          (lambda ()))
+
 (add-to-list 'load-path (expand-package "git-modes"))
 (add-to-list 'load-path (expand-package "magit"))
 (autoload 'magit-status "magit" "Loads magit-mode" t)
