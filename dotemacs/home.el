ac507ce371a88c58387dd37bd0dd1f6f4a4551d9
Bruno Jacquet <bruno.jacquet@gmail.com>
2014-07-25 18:45:35 +0100

    Sorted 'custom-set-variables' alphabetically.

--- dotemacs/home.el
+++ dotemacs/home.el
@@ -10,43 +10,42 @@
 ;;; Look & Feel
 ;;;
 (custom-set-variables
-  ;; custom-set-variables was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(case-fold-search t)
  '(current-language-environment "UTF-8")
  '(debug-on-error t)
  '(default-input-method "portuguese-prefix")
  '(global-font-lock-mode t nil (font-lock))
- ;; '(pc-selection-mode t nil (pc-select)) ; marked as obsolete
- '(show-paren-mode t nil (paren))
- '(transient-mark-mode t)
- '(user-mail-address "bruno.jacquet@gmail.com")
- '(user-full-name "Bruno Jacquet")
- '(uniquify-buffer-name-style (quote forward) nil (uniquify))
- '(tool-bar-mode nil)
- '(set-default-font "DejaVu Sans Mono-11")
- '(visible-bell t)
- '(inhibit-startup-message t)
- '(line-number-mode t)
- '(scroll-step 1)           ; scroll one line past the edge of the screen
- '(global-font-lock-mode t) ; sintax highlight
- '(transient-mark-mode t)   ; highlighted when the mark is active
- '(show-paren-mode t)       ; highlight matching parenthesis
- '(next-line-add-newlines nil)
- '(require-final-newline t)
- '(next-line-extends-end-of-buffer nil)
- '(auto-save-default nil)
- '(make-backup-files nil)
- ;'(iswitchb-mode t)         ; intelligent buffer switcher (in minibuffer)
- '(ido-enable-flex-matching t)
  '(ide-everywhere t)
- '(ido-mode t)
- '(setq kmacro-call-mouse-event nil)
+ '(ido-enable-flex-matching t)
+ '(ido-mode t nil (ido))
+ '(inhibit-startup-screen t)
+ '(line-number-mode t)
+ '(make-backup-files nil)
+ '(next-line-add-newlines nil)
+ '(next-line-extends-end-of-buffer nil)
+ '(recentf-max-saved-items nil)
  '(recentf-mode t)
- '(recentf-max-saved-items nil))
+ '(require-final-newline t)
+ '(scroll-step 1)
+ '(set-default-font "DejaVu Sans Mono-11")
+ '(setq kmacro-call-mouse-event)
+ '(show-paren-mode t nil (paren))
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(user-full-name "Bruno Jacquet")
+ '(user-mail-address "bruno.jacquet@gmail.com")
+ '(visible-bell t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom -- don't edit or cut/paste it!
-  ;; Your init file should contain only one such instance.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
 
 
