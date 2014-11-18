d050b13aad92bf6d6a4e5892d88a35117f4437d2
Bruno Jacquet <bruno.jacquet@gmail.com>
2014-11-18 01:05:32 +0000

    Added `ido-vertical-mode'.

---
+++ pkg-loaders/ido-vertical.el
@@ -0,0 +1,7 @@
+;;; ---------------------------------------------------------------------
+;;; IDO Vertical
+;;;
+(add-to-list 'load-path (expand-package "ido-vertical"))
+(require 'ido-vertical-mode)
+(ido-vertical-mode t)
+
