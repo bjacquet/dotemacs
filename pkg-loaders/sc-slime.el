40ac2e8e0214ff1d35197652286f90ca6e99e69e
B Jacquet <bjacquet@siscog.pt>
2015-05-06 12:15:42 +0100

    Corrected sc-slime loader

--- pkg-loaders/sc-slime.el
+++ pkg-loaders/sc-slime.el
@@ -3,5 +3,6 @@
 ;;;
 (ensure-package 'slime)
 (require 'slime-autoloads)
+(slime-setup '(slime-fancy))
 (setq lisp-indent-function 'common-lisp-indent-function)
 (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
