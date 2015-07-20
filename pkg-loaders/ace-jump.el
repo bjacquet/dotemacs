a815ed547199fe34cdcb9ce770aeb2fb51735969
Bruno Jacquet <bruno.jacquet@gmail.com>
2015-07-20 23:38:35 +0100

    Replaces Ace Jump with Avy Zap

--- pkg-loaders/ace-jump.el
+++
@@ -1,6 +0,0 @@
-;;; ---------------------------------------------------------------------
-;;; Ace Jump
-;;;
-(ensure-package 'ace-jump-mode)
-(require 'ace-jump-mode)
-(define-key global-map (kbd "C-S-j") 'ace-jump-mode)
---
+++ pkg-loaders/avy-zap.el
@@ -0,0 +1,6 @@
+;;; ---------------------------------------------------------------------
+;;; Avy Zap
+;;;
+(ensure-package 'avy-zap)
+(require 'avy-zap)
+(define-key global-map (kbd "C-S-j") 'avy-goto-word-or-subword-1)
