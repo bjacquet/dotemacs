1ce76e806cb705933fbfe6d3bf470dd68d701c71
B Jacquet <bjacquet@siscog.pt>
2015-09-03 10:08:47 +0100

    Added scratch.el and slime-indentation

--- pkg-loaders/sc-slime.el
+++ pkg-loaders/sc-slime.el
@@ -3,6 +3,6 @@
 ;;;
 (ensure-package 'slime)
 (require 'slime-autoloads)
-(slime-setup '(slime-fancy))
+(slime-setup '(slime-fancy slime-indentation))
 (setq lisp-indent-function 'common-lisp-indent-function)
 (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
---
+++ pkg-loaders/scratch.el
@@ -0,0 +1,5 @@
+;;; ---------------------------------------------------------------------
+;;; Scratch buffers
+;;;
+(add-to-list 'load-path (expand-package "scratch.el"))
+(require 'scratch)
