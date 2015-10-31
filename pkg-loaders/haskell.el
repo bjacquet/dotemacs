9056fb06c9953ddd581f9ab9113a1f59e8501f6b
Bruno Jacquet <bruno.jacquet@gmail.com>
2015-10-31 22:39:06 +0200

    Add haskell-mode

---
+++ pkg-loaders/haskell.el
@@ -0,0 +1,6 @@
+;;; ---------------------------------------------------------------------
+;;; Haskell
+;;;
+(ensure-package 'haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+
