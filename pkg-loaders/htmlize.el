fab375a32b26caa08b4f957eaa1c44db4c81826d
B Jacquet <bjacquet@siscog.pt>
2015-11-25 10:04:47 +0000

    Add bj:export-region-to-html
    Provided by LBO.

--- pkg-loaders/htmlize.el
+++ pkg-loaders/htmlize.el
@@ -4,7 +4,7 @@
 (ensure-package 'htmlize)
 
 (defun bj:export-buffer-to-html ()
-  "Provided by LBO. Needs tweaking! :-)"
+  "Provided by LBO."
   (interactive)
   (let ((themes custom-enabled-themes))
     (mapc #'disable-theme themes)
@@ -14,4 +14,21 @@
 	    (write-file file)
 	    (browse-url file))
 	  (kill-buffer))
+      (mapc #'enable-theme themes))))
+
+(defun bj:export-region-to-html ()
+  "Provided by LBO."
+  (interactive)
+  (let ((themes custom-enabled-themes)
+        (transient-mark-mode-enabled transient-mark-mode))
+    (mapc #'disable-theme themes)
+    (transient-mark-mode -1)
+    (redisplay)
+    (unwind-protect
+         (with-current-buffer (htmlize-region (region-beginning) (region-end))
+           (let ((file (make-temp-file "htmlized-region-" nil ".html")))
+             (write-file file)
+             (browse-url file))
+           (kill-buffer))
+      (transient-mark-mode (if transient-mark-mode-enabled 1 -1))
       (mapc #'enable-theme themes))))
