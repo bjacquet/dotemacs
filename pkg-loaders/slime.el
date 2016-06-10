b0fb26485a03a0cba93297169bbd225886e2cea3
B Jacquet <bruno.jacquet@gmail.com>
2016-06-10 23:30:42 +0200

    Updated Slime configuration

--- pkg-loaders/slime.el
+++ pkg-loaders/slime.el
@@ -2,6 +2,14 @@
 ;;; Slime - for use at home
 ;;;
 
-(load (expand-file-name "~/Documents/Projects/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl");)
+(load (expand-file-name "~/Downloads/quicklisp/slime-helper.el"))
+
+(require 'slime-autoloads)
+(slime-setup '(slime-fancy slime-asdf slime-indentation slime-banner
+               slime-tramp slime-mdot-fu slime-quicklisp))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+
+(setq lisp-indent-function 'common-lisp-indent-function)
+(setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+
+(setq common-lisp-hyperspec-root "~/Documents/References/HyperSpec/")
