92ec74d2a6f5d83ffcbb3b74a61e2a59a18a17bc
B Jacquet <brunjact@protonmail.com>
2017-01-20 22:18:46 +0100

    Haskell mode

--- pkg-loaders/haskell.el
+++ pkg-loaders/haskell.el
@@ -2,11 +2,13 @@
 ;;; Haskell
 ;;;
 (use-package haskell-mode
-   :mode "\\.hs\\'"
-   :bind (("C-c C-c" . haskell-compile)
-	  ("C-c C-l" . haskell-process-load-or-reload))
-   :config
-   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-   (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-   (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-   (setq haskell-program-name "/usr/bin/ghci -fobject-code"))
+  :ensure t
+  :commands haskell-mode
+  :init  (add-to-list 'auto-mode-alist '("\\.l?hs$" . haskell-mode))
+  :bind ("C-c C-l" . haskell-process-load-or-reload)
+  :config
+  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+  (setq haskell-program-name "/usr/bin/ghci -fobject-code"))
