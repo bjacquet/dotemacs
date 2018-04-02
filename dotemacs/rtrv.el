bff6e8401aad8a1ebb862b72cbb558852ce20bbe
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2018-04-02 13:39:20 +0100

    Improve RoR setup

--- dotemacs/rtrv.el
+++ dotemacs/rtrv.el
@@ -45,7 +45,7 @@
  '(org-hide-leading-stars t)
  '(package-selected-packages
    (quote
-    (handlebars-mode handlebars-sgml-mode slim-mode sr-speedbar yasnippet-snippets enh-ruby-mode robe smartparens shrink-whitespace org-bullets yasnippet wn-mode winpoint use-package undo-tree tangotango-theme tango-2-theme smart-mode-line rust-mode paredit pager naquadah-theme multiple-cursors markdown-mode magit ido-vertical-mode haskell-mode expand-region diminish darktooth-theme color-theme-solarized color-theme-modern chess bm birds-of-paradise-plus-theme avy auto-complete arbitools)))
+    (darkroom flyspell-popup projectile-rails projectile rinari ag handlebars-mode handlebars-sgml-mode slim-mode sr-speedbar yasnippet-snippets enh-ruby-mode robe smartparens shrink-whitespace org-bullets yasnippet wn-mode winpoint use-package undo-tree tangotango-theme tango-2-theme smart-mode-line rust-mode paredit pager naquadah-theme multiple-cursors markdown-mode magit ido-vertical-mode haskell-mode expand-region diminish darktooth-theme color-theme-solarized color-theme-modern chess bm birds-of-paradise-plus-theme avy auto-complete arbitools)))
  '(recentf-max-saved-items nil)
  '(recentf-mode t)
  '(require-final-newline t)
@@ -109,10 +109,31 @@
 (add-to-list 'ac-modes 'enh-ruby-mode)
 (add-to-list 'ac-modes 'web-mode)
 
-(use-package enh-ruby-mode :ensure t)
+(use-package enh-ruby-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist
+               '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+  (add-hook 'enh-ruby-mode-hook 'robe-mode)
+  ;; (add-hook 'enh-ruby-mode-hook 'yard-mode)
+  (add-hook 'enh-ruby-mode 'smartparens-minor-mode)
+  )
 (use-package robe :ensure t)
-(use-package smartparens :ensure t )
-
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-config)
+  (require 'smartparens-ruby)
+  (smartparens-global-mode)
+  (show-smartparens-global-mode t)
+  (sp-with-modes '(rhtml-mode)
+    (sp-local-pair "<" ">")
+    (sp-local-pair "<%" "%>")))
+(use-package ag
+  :ensure t
+  :config (setq ag-executable "/usr/local/bin/ag"))
+(use-package rinari :ensure t)
+(use-package projectile-rails :ensure t)
 
 ;;; ---------------------------------------------------------------------
 ;;; Enable disabled commands
