49fb933bf95c79447d1a76e8d526682683054bc3
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2018-04-02 13:40:08 +0100

    TODO tags and agenda files

--- pkg-loaders/org-rtrv.el
+++ pkg-loaders/org-rtrv.el
@@ -6,5 +6,17 @@
 
 (setq org-agenda-files
       '("~/Documents/Blog/"
-        "~/Projects/team-allocator/"
         "~/Documents/Diary/"))
+(load-file "org-agenda-files.el")
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "WIP(w)" "AWAITING(a)" "PENDING(p)" "|" "FIXED(f)" "SEP(s)")
+        (sequence "|" "CANCELED(c)")))
+
+(setq org-todo-keyword-faces
+      '(("TODO" . (:foreground "black" :background "red2"))
+        ("WIP" .  (:foreground "black" :background "yellow" :weigth bold))
+        ;;        ("CANCELED" . (:foreground "blue" :weight bold))
+        ("WFF" .  (:foreground "black" :background "goldenrod1"))
+        ("CANCELED" . (:foreground "green" :background "black" :weight bold))))
