189795474cc00bf776cf19bcbc31bf71bde548c0
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2018-07-20 17:51:00 +0100

    Set org TODOs for Runtime

--- pkg-loaders/org-rtrv.el
+++ pkg-loaders/org-rtrv.el
@@ -11,12 +11,17 @@
 
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)")
-        (sequence "WIP(w)" "AWAITING(a)" "PENDING(p)" "|" "FIXED(f)" "SEP(s)")
+        (sequence "WIP(w)" "AWAITING(a)" "BLOCKED(b)" "|"  "IN-REVIEW(r)" "FIXED(f)" "SEP(s)")
         (sequence "|" "CANCELED(c)")))
 
 (setq org-todo-keyword-faces
-      '(("TODO" . (:foreground "black" :background "red2"))
-        ("WIP" .  (:foreground "black" :background "yellow" :weigth bold))
-        ;;        ("CANCELED" . (:foreground "blue" :weight bold))
-        ("WFF" .  (:foreground "black" :background "goldenrod1"))
-        ("CANCELED" . (:foreground "green" :background "black" :weight bold))))
+      '(("TODO"      . (:foreground "black" :background "red2"))
+        ("WIP"       . (:foreground "black" :background "yellow" :weigth bold))
+        ("BLOCKED"   . (:foreground "white" :background "firebrick" :weight bold))
+        ("IN-REVIEW" . (:foreground "black" :background "goldenrod1"))
+        ("CANCELED"  . (:foreground "green" :background "black" :weight bold))))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
