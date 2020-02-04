0ccb905ea06517fd466458ddc36b3f951dce604c
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2020-02-04 10:27:43 +0000

    Add mu4e email reader

--- dotemacs/rtrv.el
+++ dotemacs/rtrv.el
@@ -85,6 +85,7 @@
 (bj:load-pkg-loader "bm.el")
 (bj:load-pkg-loader "color-theme.el")
 (bj:load-pkg-loader "deft.el")
+(bj:load-pkg-loader "email.el")
 (bj:load-pkg-loader "expand-region.el")
 (bj:load-pkg-loader "goto-last-change.el")
 (bj:load-pkg-loader "htmlize.el")
