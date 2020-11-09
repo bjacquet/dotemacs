aa2d6e836159b816ebab99f00279b75afe49eae6
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2020-11-09 11:39:42 +0000

    Create `bj:font-random`

--- packages/random-font.el
+++ packages/random-font.el
@@ -2,20 +2,25 @@
 ;;; Random fonts
 ;;;
 
-(let ((fonts (list "Lucida Console-13"))
-      font)
-  (if mac-p
-      (setq fonts (list "iA Writer Mono S-15"
-                        "Menlo-14"
-                        "Monaco-14"
-                        "NovaMono-15"
-                        "Anonymous Pro-16"
-                        "CozetteVector-15"
-                        "Victor Mono-15:light"
-                        ))
-    (setq fonts (list "NovaMono-12"
-                      "Share Tech Mono-12"
-                      "Ubuntu Mono-12")))
-  (setq font (nth (random (length fonts)) fonts))
-  (set-frame-font font)
-  (message (format "Random font: %s" font)))
+(defun bj:font-random ()
+  (interactive)
+
+  (let ((fonts (list "Lucida Console-13"))
+        font)
+    (if mac-p
+        (setq fonts (list "iA Writer Mono S-15"
+                          "Menlo-14"
+                          "Monaco-14"
+                          "NovaMono-15"
+                          "Anonymous Pro-16"
+                          "CozetteVector-17"
+                          "Victor Mono-15"
+                          ))
+      (setq fonts (list "NovaMono-12"
+                        "Share Tech Mono-12"
+                        "Ubuntu Mono-12")))
+    (setq font (nth (random (length fonts)) fonts))
+    (set-frame-font font)
+    (message (format "Random font: %s" font))))
+
+(bj:font-random)
