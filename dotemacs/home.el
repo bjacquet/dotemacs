0a29ec5b893d43c7c4b41529e6bd1221b7c33974
Bruno Jacquet <b.jacquet@runtime-revolution.com>
2021-06-24 15:38:17 +0100

    Add rg

--- dotemacs/home.el
+++ dotemacs/home.el
@@ -72,7 +72,6 @@
 (bj:load-pkg-loader "pager.el")
 (bj:load-pkg-loader "org.el")
 (bj:load-pkg-loader "paredit.el")
-(bj:load-pkg-loader "rg.el")
 (bj:load-pkg-loader "shrink-whitespace.el")
 (bj:load-pkg-loader "smart-mode-line.el")
 (bj:load-pkg-loader "undo-tree.el")
