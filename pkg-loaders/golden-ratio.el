678f18fef14af0b564179e7a4ffe9c3690abc000
Bruno Jacquet <bjacquet@komodo.local>
2024-11-29 22:20:39 +0000

    Removed unneeded files

--- pkg-loaders/golden-ratio.el
+++
@@ -1,6 +0,0 @@
-;;; ---------------------------------------------------------------------
-;;; Golden Ratio
-;;;
-(use-package golden-ratio
-  :ensure t
-  :diminish golden-ratio)
--- pkg-loaders/org.el
+++
@@ -1,7 +0,0 @@
-;;; ---------------------------------------------------------------------
-;;; Org-mode
-;;;
-(use-package magit-org-todos
-  :ensure t
-  :config
-  (magit-org-todos-autoinsert))
