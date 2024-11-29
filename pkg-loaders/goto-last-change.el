903d8b2659ac11a6fbb0b852b40d32f21c24a4b3
Bruno Jacquet <bjacquet@komodo.local>
2024-11-29 10:38:03 +0000

    Goodbye goto-last-change
    I never used you much.

--- pkg-loaders/goto-last-change.el
+++
@@ -1,7 +0,0 @@
-;;; ---------------------------------------------------------------------
-;;; Goto Last Change
-;;;
-(use-package goto-last-change
-  :ensure t
-  :demand t
-  :bind ("C-c \\" . goto-last-change))
